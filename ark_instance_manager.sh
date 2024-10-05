98028ddaeff4d300733e0008f609e84d27f7510f
Zerschranzer <zerschranzer@googlemail.com>
2024-10-05 22:07:07 +0200

    Update ark_instance_manager.sh

--- ark_instance_manager.sh
+++ ark_instance_manager.sh
@@ -129,7 +129,7 @@ check_dependencies() {
                 ;;
         esac
 
-        echo -e "${YELLOW}Continue anyway?${RESET} ${RED}(not recommended) [y/N]${RESET}"
+        echo -e "${YELLOW}Continue anyway?${RESET} ${RED}(not recommended)${RESET} ${YELLOW}[y/N]${RESET}"
         read -r response
         if [[ ! $response =~ ^[Yy]$ ]]; then
             echo -e "${RED}Exiting due to missing dependencies.${RESET}"
